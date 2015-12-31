56fcad13e03c977569955de6180db3e78366fc0b
Alex McRitchie <amcritchie@gmail.com>
2015-12-31 00:09:39 -0700

    Cleaning up user edit modal and phone filter directive.

--- app/assets/javascripts/admin/controllers/UsersCtrl.js
+++ app/assets/javascripts/admin/controllers/UsersCtrl.js
@@ -9,12 +9,10 @@ admin_app.controller('UsersCtrl', function($scope, $http, $location, $resource) 
 
   $scope.edit = function(user){
     User.get({ id: user.id}, function(res) {
-      debugger;
       $scope.user = res
     });
   }
   $scope.update = function(user){
-    debugger;
     User.update({ id: user.id }, user, function(res) {
       console.log('user updated');
     })
